fd021aeb2c4f578ce843cd7eab343780e0ef9e90
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-23 08:35:48 +0100

    Update study.jl

--- src/study.jl
+++ src/study.jl
@@ -80,6 +80,17 @@ function study(q::Int, Q::Int, type::Symbol, Δ::Float64=0.0, prange = 0.01:0.00
             end
         end
     end
+    println("q=$(q), Q=$(Q), type=$(type), Δ=$(Δ)")
+    println("-"^30)
+    println("phase\t| %")
+    println("-"^30)
+    println("IGP\t|", round(100*count(phase .== 4)/length(phase), digits=9))
+    println("BGP\t|", round(100*count(phase .== 3)/length(phase), digits=9))
+    println("Comp.\t|", round(100*count(phase .== 2)/length(phase), digits=9))
+    println("PC\t|", round(100*count(phase .== 1)/length(phase), digits=9))
+    println("MGC\t|", round(100*count(phase .== 0)/length(phase), digits=9))
+    println("unknown\t|", round(100*count(phase .== -1)/length(phase), digits=9))
+    println("-"^30)
     return c, μ, phase, prange, βrange
 end
 
