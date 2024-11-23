769b7d8c0f41fcb76af180341462489079ed1a97
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-23 12:27:41 +0100

    Update study.jl

--- src/study.jl
+++ src/study.jl
@@ -62,19 +62,19 @@ function study(q::Int, Q::Int, type::Symbol, Δ::Float64=0.0, prange = 0.01:0.00
                     
             # classify phases
             if μA > 0.5 && μB > 0.5 && μG > 0.5
-                phase[i, j] = 5 # in-group polarization
+                phase[i, j] = 4 # in-group polarization
             elseif μA < 0.5 && μB < 0.5 && μG > 0.5
-                phase[i, j] = 4 # between-group polarization
-            elseif (μA > 0.5 || μB > 0.5) && μG < 0.5 
-                phase[i, j] = 3 # compromise    
+                phase[i, j] = 3 # between-group polarization
             elseif μA < 0.5 && μB < 0.5 && μG < 0.5
                 if (c1 > (c2-tol) && c1 > (c3-tol)) || (c3 > (c2-tol) && c3 > (c1-tol))
                     phase[i, j] = 2 # pole consensus
                 else
                     phase[i, j] = 1 # middle-ground consensus
                 end
+            elseif (μA > 0.5 || μB > 0.5) && μG < 0.5 
+                phase[i, j] = 0 # mixed
             else
-                # warn if the phase is unclassified
+                phase[i, j] = -1 # unclassified
                 @warn "unclassified phase at (p=$(prange[i]), β=$(βrange[j])) with cA=$((c1A, c2A, c3A)), cB=$((c1B, c2B, c3B)))"
             end
         end
