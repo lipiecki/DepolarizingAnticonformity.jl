be0afe65d7cb58a619101a08119da655be725aa0
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-24 23:11:36 +0100

    Update study.jl

--- src/study.jl
+++ src/study.jl
@@ -16,7 +16,7 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
             for j in eachindex(probability_outgroup)
                 β = probability_outgroup[j]
                 prob = ODEProblem(de, c0, (0, T), [p, β])
-                sol = solve(prob, Rosenbrock23(), saveat=[T/10, T])
+                sol = solve(prob, Rosenbrock23(), saveat=[T/10, T], abstol=1e-8, reltol=1e-8)
                 for var in 1:4 # stationarity test
                     abs(sol.u[1][var] - sol.u[2][var]) < tol || @warn "System did not converge for p = $(p), and β = $(β) with the difference of $(abs(sol.u[1][var] - sol.u[2][var]))"
                 end
@@ -29,7 +29,7 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
             for j in eachindex(probability_outgroup)
                 β = probability_outgroup[j]
                 prob = ODEProblem(de, [c0[1]*p, c0[2]*p, c0[1]*(1-p), c0[2]*(1-p), c0[3]*p, c0[4]*p, c0[3]*(1-p), c0[4]*(1-p)], (0, T), [p, β])
-                sol = solve(prob, Rosenbrock23(), saveat=[T/10, T])
+                sol = solve(prob, Rosenbrock23(), saveat=[T/10, T], abstol=1e-8, reltol=1e-8)
                 for var in 1:8 # stationarity test
                     abs(sol.u[1][var] - sol.u[2][var]) < tol || @warn "System did not converge for p = $(p), and β = $(β) with the difference of $(abs(sol.u[1][var] - sol.u[2][var]))"
                 end
