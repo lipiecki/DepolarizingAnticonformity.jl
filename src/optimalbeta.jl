3a71dca074903e1ce9c3b3b91afacdeb61e7e272
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-24 14:02:17 +0100

    Create optimalbeta.jl

---
+++ src/optimalbeta.jl
@@ -0,0 +1,15 @@
+function optimalbeta(q::Int, Q::Int)
+    optimalβ = ones(3)
+    for (no, type) in enumerate((:dynamic1, :dynamic2, :static2))
+        data = load(joinpath("DepolarizingAnticonformityResults", "OutputFiles", "q$(q)_Q$(Q)_$(type).jld2"))
+        p = 1.0
+        for i in axes(data["polarization_index"], 1)
+            for j in axes(data["polarization_index", 2)
+                if data["phase"][i, j] == -2 && data["probability_outgroup"][j] < p+1e-12
+                    optimalβ[no] = data["intervention_strength"][i]
+                end
+            end
+        end
+    end
+    return optimalβ
+end
