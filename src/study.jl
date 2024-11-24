44e8ffc68433efee863c4a70ebef37269dea65d4
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-24 18:03:19 +0100

    Update study.jl

--- src/study.jl
+++ src/study.jl
@@ -15,8 +15,8 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
             p = intervention_strength[i]
             for j in eachindex(probability_outgroup)
                 β = probability_outgroup[j]
-                prob = ODEProblem(de, c0, (0, 2T), [p, β])
-                sol = solve(prob, Rosenbrock23(), saveat=[T, 2T])
+                prob = ODEProblem(de, c0, (0, T), [p, β])
+                sol = solve(prob, Rosenbrock23(), saveat=[T//2, T])
                 for var_index in eachindex(sol(1)) # stationarity test
                     if abs(sol(2)[var_index] - sol(1)[var_index]) > tol
                         @warn "System did not converge for p = $(p), and frac = $(β)" 
@@ -30,8 +30,8 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
             p = intervention_strength[i]
             for j in eachindex(probability_outgroup)
                 β = probability_outgroup[j]
-                prob = ODEProblem(de, [c0[1]*p, c0[2]*p, c0[1]*(1-p), c0[2]*(1-p), c0[3]*p, c0[4]*p, c0[3]*(1-p), c0[4]*(1-p)], (0, 2T), [p, β])
-                sol = solve(prob, Rosenbrock23(), saveat=[T, 2T])
+                prob = ODEProblem(de, [c0[1]*p, c0[2]*p, c0[1]*(1-p), c0[2]*(1-p), c0[3]*p, c0[4]*p, c0[3]*(1-p), c0[4]*(1-p)], (0, T), [p, β])
+                sol = solve(prob, Rosenbrock23(), saveat=[T//2, T])
                 for var_index in eachindex(sol(1)) # stationarity test
                     if abs(sol(2)[var_index] - sol(1)[var_index]) > tol
                         @warn "System did not converge for p = $(p), and frac = $(β)" 
