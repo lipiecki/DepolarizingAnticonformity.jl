84601f429d8808e2ff3fb15b1454c9f4a19e4fde
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-24 21:34:06 +0100

    Update study.jl

--- src/study.jl
+++ src/study.jl
@@ -15,13 +15,9 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
             p = intervention_strength[i]
             for j in eachindex(probability_outgroup)
                 β = probability_outgroup[j]
-                prob = ODEProblem(de, c0, (T/10, T), (p, β))
-                sol = solve(prob, Rosenbrock23(), saveat=(T/10, T))
-                for var in 1:4 # stationarity test
-                    if abs(sol.u[1][var] - sol.u[2][var]) > tol
-                        @warn "System did not converge for p = $(p), and β = $(β)"
-                    end
-                end
+                prob = ODEProblem(de, c0, (T, 2T), (p, β))
+                sol = solve(prob, Rosenbrock23(), saveat=(T, 2T))
+                sum(abs(sol.u[1] .- sol.u[2]) < tol || @warn "System did not converge for p = $(p), and β = $(β)" # stationarity test
                 c[i, j, :] .= @view(sol.u[2])
             end
         end
@@ -30,13 +26,9 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
             p = intervention_strength[i]
             for j in eachindex(probability_outgroup)
                 β = probability_outgroup[j]
-                prob = ODEProblem(de, [c0[1]*p, c0[2]*p, c0[1]*(1-p), c0[2]*(1-p), c0[3]*p, c0[4]*p, c0[3]*(1-p), c0[4]*(1-p)], (T/10, T), (p, β))
-                sol = solve(prob, Rosenbrock23(), saveat=(T/10, T))
-                for var in 1:8 # stationarity test
-                    if abs(sol.u[1][var] - sol.u[2][var]) > tol
-                        @warn "System did not converge for p = $(p), and β = $(β)"
-                    end
-                end
+                prob = ODEProblem(de, [c0[1]*p, c0[2]*p, c0[1]*(1-p), c0[2]*(1-p), c0[3]*p, c0[4]*p, c0[3]*(1-p), c0[4]*(1-p)], (T, 2T), (p, β))
+                sol = solve(prob, Rosenbrock23(), saveat=(T, 2T))
+                sum(abs(sol.u[1] .- sol.u[2]) < tol || @warn "System did not converge for p = $(p), and β = $(β)" # stationarity test
                 c[i, j, 1] = sol.u[2][1] + sol.u[2][3] 
                 c[i, j, 2] = sol.u[2][2] + sol.u[2][4]
                 c[i, j, 3] = sol.u[2][5] + sol.u[2][7]
