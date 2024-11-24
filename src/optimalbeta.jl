2066c5b9bdeb9a3acfabf2c779616e4866b3000b
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-24 14:54:30 +0100

    Update optimalbeta.jl

--- src/optimalbeta.jl
+++ src/optimalbeta.jl
@@ -1,21 +1,34 @@
 function optimalbeta(q::Int, Q::Int)
-    optimalβl = ones(3)
-    optimalβu = ones(3)
-    for (no, type) in enumerate((:dynamic1, :dynamic2, :static2))
+    for (no, type) in enumerate((:dynamic2, :static2))
         data = load(joinpath("DepolarizingAnticonformityResults", "OutputFiles", "q$(q)_Q$(Q)_$(type).jld2"))
+        βc = 0.0
         p = 1.0
         for i in axes(data["polarization_index"], 1)
             for j in axes(data["polarization_index"], 2)
                 if data["phase"][i, j] == -2
-                    if data["intervention_strength"][i] < p - 1e-9
-                        optimalβu[no] = data["probability_outgroup"][j]
+                    if !(data["intervention_strength"][i] ≈ p) && data["intervention_strength"][i] < p 
+                        βc = data["probability_outgroup"][j]
                         p = data["intervention_strength"][i]
-                    elseif data["intervention_strength"][i] ≈ p
-                        optimalβl[no] = data["probability_outgroup"][j]
                     end
                 end
             end
         end
+        println("Critical β for $(type): $(βc)")
     end
-    return optimalβl, optimalβu
+    type = :dynamic1
+    data = load(joinpath("DepolarizingAnticonformityResults", "OutputFiles", "q$(q)_Q$(Q)_$(type).jld2"))
+    βl, βu = 0.0, 0.0
+    p = 1.0
+    for i in axes(data["polarization_index"], 1)
+        for j in axes(data["polarization_index"], 2)
+            if data["phase"][i, j] == -2
+                if (data["intervention_strength"][i] ≈ p)
+                    βu = data["probability_outgroup"][j]
+                elseif data["intervention_strength"][i] < p
+                    βl = data["probability_outgroup"][j]
+                end
+            end
+        end
+    end
+    println("Optimal β for $(type): [$(βu), $("βu")")
 end
