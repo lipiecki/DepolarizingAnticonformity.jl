7924c898e203c66381d7858dc247dabdaff896ce
Arkadiusz Lipiecki <158849276+lipiecki@users.noreply.github.com>
2024-11-24 22:11:11 +0100

    Update study.jl

--- src/study.jl
+++ src/study.jl
@@ -17,9 +17,9 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
                 β = probability_outgroup[j]
                 prob = ODEProblem(de, c0, (T/10, T), [p, β])
                 sol = solve(prob, Rosenbrock23(), saveat=[T/10, T])
-                for var in 1:4
-                     abs(sol.u[1][var] - sol.u[2][var]) < tol || @warn "System did not converge for p = $(p), and β = $(β)" # stationarity test
-                end
+                #for var in 1:4
+                #     abs(sol.u[1][var] - sol.u[2][var]) < tol || @warn "System did not converge for p = $(p), and β = $(β)" # stationarity test
+                #end
                 c[i, j, :] .= sol.u[2]
             end
         end
@@ -30,9 +30,9 @@ function study(q::Int, Q::Int, type::Symbol; Δ::Float64=0.0, intervention_stren
                 β = probability_outgroup[j]
                 prob = ODEProblem(de, [c0[1]*p, c0[2]*p, c0[1]*(1-p), c0[2]*(1-p), c0[3]*p, c0[4]*p, c0[3]*(1-p), c0[4]*(1-p)], (T/10, T), [p, β])
                 sol = solve(prob, Rosenbrock23(), saveat=[T/10, T])
-                for var in 1:8
-                     abs(sol.u[1][var] - sol.u[2][var]) < tol || @warn "System did not converge for p = $(p), and β = $(β)" # stationarity test
-                end
+                #for var in 1:8
+                #     abs(sol.u[1][var] - sol.u[2][var]) < tol || @warn "System did not converge for p = $(p), and β = $(β)" # stationarity test
+                #end
                 c[i, j, 1] = sol.u[2][1] + sol.u[2][3] 
                 c[i, j, 2] = sol.u[2][2] + sol.u[2][4]
                 c[i, j, 3] = sol.u[2][5] + sol.u[2][7]
