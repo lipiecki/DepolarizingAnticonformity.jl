553a78b4aabffd4285dd3eafe5436d571dc12e1a
lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2025-01-22 11:50:30 +0100

    main

---
+++ src/maps.jl
@@ -0,0 +1,21 @@
+function polarizationmap(q::Int, Q::Int, type::Symbol)
+    data = load(joinpath("DepolarizingAnticonformityResults", "OutputFiles", "q$(q)_Q$(Q)_$(type).jld2"))
+    plt = heatmap(data["intervention_strength"], data["probability_outgroup"], data["polarization_index"]', c=cgrad(:RdBu, rev=true), clims=(0, 1))
+    plot!(plt, framestyle=:grid, colorbar=false, size=(300, 300), xlabel="p", ylabel="β")
+    savefig(plt, joinpath(mkpath(joinpath("DepolarizingAnticonformityResults", "Figures")), "polarization_q$(q)_Q$(Q)_$(type).pdf"))
+end
+
+function phasemap(q::Int, Q::Int, type::Symbol)
+    data = load(joinpath("DepolarizingAnticonformityResults", "OutputFiles", "q$(q)_Q$(Q)_$(type).jld2"))
+    plt = heatmap(data["intervention_strength"], data["probability_outgroup"], data["phase"]', c=cgrad(:RdBu, 5, categorical=true, rev=true), clims=(-2, 2))
+    plot!(plt, framestyle=:grid, colorbar=false, size=(300, 300), xlabel="p", ylabel="β")
+    mkpath(joinpath("DepolarizingAnticonformityResults", "Figures"))
+    savefig(plt, joinpath(mkpath(joinpath("DepolarizingAnticonformityResults", "Figures")), "phase_q$(q)_Q$(Q)_$(type).pdf"))
+end
+
+function sensitivitymap(q::Int, Q::Int, type::Symbol)
+    data = load(joinpath("DepolarizingAnticonformityResults", "OutputFiles", "sensitivity_q$(q)_Q$(Q)_$(type).jld2"))
+    plt = heatmap(data["intervention_strength"], data["probability_outgroup"], data["sensitivity"]', c=cgrad(:Blues, 6, categorical=true), clims=(0, 5))
+    plot!(plt, framestyle=:grid, colorbar=true, size=(330, 300), xlabel="p", ylabel="β")
+    savefig(plt, joinpath(mkpath(joinpath("DepolarizingAnticonformityResults", "Figures")), "sensitivity_q$(q)_Q$(Q)_$(type).pdf"))
+end
